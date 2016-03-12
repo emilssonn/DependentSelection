f9da60f0af2c4dcb98f19c0688f6bf179ef5a466
Peter Emilsson <peter.emilsson@hotmail.com>
2016-03-12 14:33:30 +0100

    Added attribute class and required nuget packages.

---
+++ Src/EPiLinkedSelection/LinkedSelectOneAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using EPiServer.Shell.ObjectEditing;
+
+namespace EPiLinkedSelection
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class LinkedSelectOneAttribute : Attribute, IMetadataAware
+    {
+        public void OnMetadataCreated(ModelMetadata metadata)
+        {
+            ExtendedMetadata extendedMetadata = metadata as ExtendedMetadata;
+            if (extendedMetadata == null)
+                return;
+
+            extendedMetadata.ClientEditingClass = "epi-linked-selection/LinkedSelectionEditor";
+        }
+    }
+}
