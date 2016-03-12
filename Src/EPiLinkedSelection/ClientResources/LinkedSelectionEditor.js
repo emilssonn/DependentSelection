21b28fea53db123530e302a3f93d647ea103a6fc
Peter Emilsson <peter.emilsson@hotmail.com>
2016-03-12 14:19:26 +0100

    Initial commit

---
+++ Src/EPiLinkedSelection/ClientResources/LinkedSelectionEditor.js
@@ -0,0 +1,29 @@
+﻿define([
+    "dojo/_base/declare",
+    "dojo/_base/connect",
+    "dojo/request",
+
+    "epi-cms/contentediting/editors/SelectionEditor"
+],
+function (
+    declare,
+    connect,
+    request,
+
+    SelectionEditor
+) {
+    return declare([SelectionEditor], {
+        constructor: function (params) {
+            this.inherited(arguments, [params]);
+        },
+        _handleOnChange: function (value, priorityChange) {
+            this.inherited(arguments, [value, priorityChange]);
+        },
+        destroy: function () {
+            this.inherited(arguments);
+        },
+        _update: function (value) {
+
+        }
+    });
+});
