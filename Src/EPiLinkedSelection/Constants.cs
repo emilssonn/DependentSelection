eef6c51dceab7a47e49f58b952c5e5f44644d266
Peter Emilsson <peter.emilsson@hotmail.com>
2016-03-13 14:46:00 +0100

    Added constants.

---
+++ Src/EPiLinkedSelection/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace EPiLinkedSelection
+{
+    public static class Constants
+    {
+        public const string DependsOn = "dependsOn";
+
+        public const string ReadOnlyWhen = "readOnlyWhen";
+
+        public const string IncludeValueFrom = "includeValueFrom";
+
+        public const string Namespace = "namespace";
+
+        public const string Values = "values";
+
+        public const string Selections = "selections";
+    }
+}
--- Src/EPiLinkedSelection/LinkedSelectOneAttribute.cs
+++ Src/EPiLinkedSelection/LinkedSelectOneAttribute.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using EPiServer.Cms.Shell.UI.ObjectEditing;
 using EPiServer.Shell.ObjectEditing;
 
 namespace EPiLinkedSelection
@@ -7,13 +10,35 @@ namespace EPiLinkedSelection
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class LinkedSelectOneAttribute : Attribute, IMetadataAware
     {
+        public string DependsOn { get; set; }
+
+        public string ReadOnlyWhen { get; set; }
+
+        public string IncludeValueFrom { get; set; }
+
         public void OnMetadataCreated(ModelMetadata metadata)
         {
-            ExtendedMetadata extendedMetadata = metadata as ExtendedMetadata;
-            if (extendedMetadata == null)
+            var contentDataMetadata = metadata as ContentDataMetadata;
+            if (contentDataMetadata == null || contentDataMetadata.OwnerContent == null)
                 return;
 
-            extendedMetadata.ClientEditingClass = "epi-linked-selection/LinkedSelectionEditor";
+            contentDataMetadata.ClientEditingClass = "epi-linked-selection/LinkedSelectionEditor";
+
+            contentDataMetadata.EditorConfiguration[Constants.Namespace] = contentDataMetadata.ContainerType.FullName;
+
+            contentDataMetadata.EditorConfiguration[Constants.DependsOn] = DependsOn;
+
+            contentDataMetadata.EditorConfiguration[Constants.ReadOnlyWhen] = ReadOnlyWhen;
+
+            contentDataMetadata.EditorConfiguration[Constants.Selections] = new List<SelectItem> { new SelectItem() { Text = "", Value = "" } };
+
+            contentDataMetadata.EditorConfiguration[Constants.IncludeValueFrom] = IncludeValueFrom;
+
+            var u = new Dictionary<string, object>();
+            var t = contentDataMetadata.OwnerContent.Property.FirstOrDefault(p => p.Name == IncludeValueFrom);
+            u.Add(t.Name, t.Value);
+            contentDataMetadata.EditorConfiguration[Constants.Values] = u;
+            
         }
     }
 }
